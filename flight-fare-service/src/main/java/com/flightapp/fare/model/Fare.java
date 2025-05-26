cf5a8a377e100c5af1b1d46ab568d82b542825cd
thrishaa7 <152695002+thrishaa7@users.noreply.github.com>
2025-05-26 14:03:25 +0530

    Add files via upload

---
+++ flight-fare-service/src/main/java/com/flightapp/fare/model/Fare.java
@@ -0,0 +1,54 @@
+package com.flightapp.fare.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "fares")
+
+public class Fare {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String flightNumber;
+	private String flightDate;
+	private Double fare;
+
+	public Fare() {}
+
+	public Fare(String flightNumber, String flightDate, Double fare) {
+	    this.flightNumber = flightNumber;
+	    this.flightDate = flightDate;
+	    this.fare = fare;
+	}
+
+	public Long getId() {
+	    return id;
+	}
+
+	public String getFlightNumber() {
+	    return flightNumber;
+	}
+
+	public void setFlightNumber(String flightNumber) {
+	    this.flightNumber = flightNumber;
+	}
+
+	public String getFlightDate() {
+	    return flightDate;
+	}
+
+	public void setFlightDate(String flightDate) {
+	    this.flightDate = flightDate;
+	}
+
+	public Double getFare() {
+	    return fare;
+	}
+
+	public void setFare(Double fare) {
+	    this.fare = fare;
+	}
+
+
+}
