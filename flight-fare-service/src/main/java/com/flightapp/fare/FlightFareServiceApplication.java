3daee0735326516841659c18b0ad33c0970cd3c0
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-05-26 16:17:57 +0530

    Add files via upload

---
+++ flight-fare-service/src/main/java/com/flightapp/fare/FlightFareServiceApplication.java
@@ -0,0 +1,15 @@
+package com.flightapp.fare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class FlightFareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFareServiceApplication.class, args);
+	}
+
+}
