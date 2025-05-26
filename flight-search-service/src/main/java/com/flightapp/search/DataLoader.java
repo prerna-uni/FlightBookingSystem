3166535f12dc2c9efca4ebe4c82ee47ef984fbb0
thrishaa7 <152695002+thrishaa7@users.noreply.github.com>
2025-05-26 14:01:56 +0530

    Add files via upload

---
+++ flight-search-service/src/main/java/com/flightapp/search/DataLoader.java
@@ -0,0 +1,27 @@
+package com.flightapp.search;
+
+import com.flightapp.search.model.Flight;
+import com.flightapp.search.repository.FlightRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final FlightRepository flightRepository;
+
+    public DataLoader(FlightRepository flightRepository) {
+        this.flightRepository = flightRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Clear old data if any
+        flightRepository.deleteAll();
+
+        // Add some sample flights
+        flightRepository.save(new Flight(null, "DEL", "MUM", "AI101", "2025-06-01", 50));
+        flightRepository.save(new Flight(null, "DEL", "BLR", "AI102", "2025-06-01", 40));
+        flightRepository.save(new Flight(null, "MUM", "DEL", "AI103", "2025-06-02", 60));
+    }
+}
