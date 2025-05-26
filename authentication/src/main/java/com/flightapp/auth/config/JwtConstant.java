d7130016a1180f91e293dadafab6c3803a733777
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-05-26 13:40:17 +0530

    Add files via upload

---
+++ authentication/src/main/java/com/flightapp/auth/config/JwtConstant.java
@@ -0,0 +1,9 @@
+package com.flightapp.auth.config;
+
+public class JwtConstant {
+
+	public static final String JWT_HEADER = "Authorization";
+	public static final String SECRET_KEY = "abjfhifdbvnxcdjkhkjkjhfghjkilcbhjtyujkfnvvlkkhgtfggdyugweiyruafcvhdbhfjhwftdfionp";
+
+
+}
