26911d1a778df34e09a8ccf5d0cedc9c7f32ffc9
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-05-29 21:03:08 +0530

    Add files via upload

---
+++ flight-checkin-service/src/main/java/com/flightapp/checkin/FlightCheckinServiceApplication.java
@@ -0,0 +1,15 @@
+package com.flightapp.checkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@SpringBootApplication
+@EnableFeignClients
+
+public class FlightCheckinServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightCheckinServiceApplication.class, args);
+	}
+
+}
