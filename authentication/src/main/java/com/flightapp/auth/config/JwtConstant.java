a063535b24fe11b976a0f6c50e86fdb041fe8096
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-05-29 20:57:56 +0530

    Delete authentication directory

--- authentication/src/main/java/com/flightapp/auth/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.flightapp.auth.config;
-
-public class JwtConstant {
-
-	public static final String JWT_HEADER = "Authorization";
-	public static final String SECRET_KEY = "abjfhifdbvnxcdjkhkjkjhfghjkilcbhjtyujkfnvvlkkhgtfggdyugweiyruafcvhdbhfjhwftdfionp";
-
-
-}
