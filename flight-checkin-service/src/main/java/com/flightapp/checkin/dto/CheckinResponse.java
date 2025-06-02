e94a026363c6490db109eba5c5e89b49aa349e00
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-06-02 13:12:56 +0530

    Add files via upload

---
+++ flight-checkin-service/src/main/java/com/flightapp/checkin/dto/CheckinResponse.java
@@ -0,0 +1,19 @@
+package com.flightapp.checkin.dto;
+
+public class CheckinResponse {
+    private String message;
+    private String confirmationNumber;
+
+    public CheckinResponse(String message, String confirmationNumber) {
+        this.message = message;
+        this.confirmationNumber = confirmationNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getConfirmationNumber() {
+        return confirmationNumber;
+    }
+}
