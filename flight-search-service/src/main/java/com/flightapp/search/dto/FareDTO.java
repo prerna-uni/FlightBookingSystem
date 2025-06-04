02eb866c952fbed4f49fbe9134b39b240fd7c4df
thrishaa7 <152695002+thrishaa7@users.noreply.github.com>
2025-06-04 12:17:31 +0530

    Add files via upload

---
+++ flight-search-service/src/main/java/com/flightapp/search/dto/FareDTO.java
@@ -0,0 +1,22 @@
+package com.flightapp.search.dto;
+
+public class FareDTO {
+    private Double fare;
+
+    public FareDTO() {}
+
+    public Double getFare() {
+        return fare;
+    }
+
+    public void setFare(Double fare) {
+        this.fare = fare;
+    }
+    @Override
+    public String toString() {
+        return "FareDTO{" +
+               "fare=" + fare +
+               '}';
+    }
+
+}
