665632c035887acdb61ed6a126dcbe7bb4fbd3f5
thrishaa7 <152695002+thrishaa7@users.noreply.github.com>
2025-06-12 23:13:08 +0530

    Add files via upload

---
+++ payment-service/src/main/java/com/flightapp/payment/dto/PaymentResponse.java
@@ -0,0 +1,22 @@
+package com.flightapp.payment.dto;
+
+public class PaymentResponse {
+    private String orderId;
+    private String razorpayKey;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getRazorpayKey() {
+        return razorpayKey;
+    }
+
+    public void setRazorpayKey(String razorpayKey) {
+        this.razorpayKey = razorpayKey;
+    }
+}
