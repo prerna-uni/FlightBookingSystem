4a7caf43d443b030bb2c8783e14c050b27f2eefe
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-06-28 13:14:15 +0530

    Delete flight-booking-service directory

--- flight-booking-service/src/main/java/com/flightapp/booking/repository/PassengerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.flightapp.booking.repository;
-
-import com.flightapp.booking.model.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
