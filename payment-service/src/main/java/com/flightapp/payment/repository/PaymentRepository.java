922217e810fd94ad8eb060c019800d593e574121
prerna-uni <prerna.20211cai0063@presidencyuniversity.in>
2025-07-31 21:43:43 +0530

    Delete payment-service directory

--- payment-service/src/main/java/com/flightapp/payment/repository/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.flightapp.payment.repository;
-
-import com.flightapp.payment.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    Payment findByBookingReference(String bookingReference);
-}
